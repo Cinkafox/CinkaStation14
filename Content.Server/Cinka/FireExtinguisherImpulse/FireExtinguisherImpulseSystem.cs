79e2c6d79910f2fe43f98809567ee9e68bce12db
Cinkafox <leofoxm40@gmail.com>
2023-03-28 20:33:11 +0300

    Added throwing on pshik

--- Content.Server/Cinka/FireExtinguisherImpulse/FireExtinguisherImpulseSystem.cs
+++ Content.Server/Cinka/FireExtinguisherImpulse/FireExtinguisherImpulseSystem.cs
@@ -1,13 +1,15 @@
-﻿using Content.Server.Extinguisher;
+﻿using Content.Server.Chemistry.Components.SolutionManager;
 using Content.Server.Fluids.Components;
 using Content.Server.Fluids.EntitySystems;
-using Content.Shared.Interaction;
+using Content.Shared.Throwing;
+using Content.Shared.Vehicle.Components;
+using Robust.Shared.Physics.Components;
 
 namespace Content.Server.Cinka.FireExtinguisherImpulse;
 
 public sealed class FireExtinguisherImpulseSystem : EntitySystem
 {
-    //[Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly ThrowingSystem _throwing = default!;
 
     public override void Initialize()
     {
@@ -17,7 +19,13 @@ public sealed class FireExtinguisherImpulseSystem : EntitySystem
 
     private void OnPshikAttempt(EntityUid uid, SprayComponent component, SprayAttemptEvent args)
     {
+        if (TryComp<RiderComponent>(args.User, out var rider) && !args.Cancelled && TryComp<SolutionContainerManagerComponent>(uid,out var scmc))
+        {
+            if (rider.Vehicle != null && TryComp<PhysicsComponent>(rider.Vehicle.Value, out var physics) && scmc.Solutions["spray"].Volume > 0)
+            {
+                _throwing.TryThrow(rider.Vehicle.Value, physics.LinearVelocity, user: args.User, pushbackRatio: 50f);
+            }
 
-        Logger.Debug("Pshik");
+        }
     }
 }
