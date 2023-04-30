041de089d501a5607d565ddb53f515dd4ba4a892
Cinka <leofoxm40@gmail.com>
2023-04-30 20:01:11 +0300

    и

---
+++ Content.Server/Cinka/Vampire/Components/VampireComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server.Cinka.Vampire.Components;
+
+[RegisterComponent]
+public sealed class VampireComponent : Component
+{
+    [ViewVariables]
+    [DataField("Bloodtype")]
+    public string Bloodtype = "Blood";
+}
---
+++ Content.Server/Cinka/Vampire/VampireSystem.cs
@@ -0,0 +1,29 @@
+﻿using Content.Server.Body.Components;
+using Content.Server.Cinka.Vampire.Components;
+using Content.Shared.Verbs;
+
+namespace Content.Server.Cinka.Vampire;
+
+
+public sealed class VampireSystem : EntitySystem
+{
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<BloodstreamComponent,GetVerbsEvent<Verb>>(OnVerb);
+    }
+
+    private void OnVerb(EntityUid uid, BloodstreamComponent component, GetVerbsEvent<Verb> args)
+    {
+        Verb v = new()
+        {
+            Text = "Высосать кровь",
+            Message = "Высосать кровь из жертвы",
+            Act = () =>
+            {
+
+            }
+        };
+
+    }
+}
