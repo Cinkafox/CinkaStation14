a365338b39ae97a54e64027d8831ac867d957e08
Cinka <leofoxm40@gmail.com>
2023-05-12 10:14:53 +0300

    Race Job Whitelist

---
+++ Content.Server/Cinka/RaceJobWhitelist/RaceJobWhiteListPrototype.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Server.Cinka.RaceJobWhitelist;
+
+[Prototype("racejobwhitelist")]
+public sealed class RaceJobWhiteListPrototype : IPrototype
+{
+    [ViewVariables]
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    //list of races that are allowed to work at this job
+    [DataField("values")] public IReadOnlyList<string> Values { get; } = new List<string>();
+}
